00249a1693a8031295f3cc8601a8b2665170b12f
Sindre Sorhus <sindresorhus@gmail.com>
2013-11-19 22:51:27 +0100

    pretty up the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,21 @@
-// Agency code.
-
 (function() {
-$( document ).ready( function() {
+  'use strict';
+  var setMenuBackgroundHeight = function () {
+    var d = $(document).height();
+    var w = $(window).width();
 
-  //
-  $('pre code').addClass('prettyprint');
-  //
-  $( window ).bind( 'load resize', setMenuBackgroundHeight );
+    if (w > 760) {
+      $('nav').css({'min-height': (d)});
+    } else {
+      $('nav').css({'min-height': (100)});
+    }
+  };
 
-  prettyPrint();
+  $(document).ready(function () {
+    $('pre code').addClass('prettyprint');
+    $(window).bind('load resize', setMenuBackgroundHeight);
 
-  setMenuBackgroundHeight();
-
-});
-
-
-var setMenuBackgroundHeight = function() {
-
-  var d = $( document ).height();
-  var w = $( window ).width();
-
-  if( w > 760 ) {
-    $( 'nav' ).css( { "min-height": (d) } );
-  } else {
-    $( 'nav' ).css( { "min-height": (100) } );
-  }
-
-};
-
+    prettyPrint();
+    setMenuBackgroundHeight();
+  });
 })();
