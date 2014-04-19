41170fdb70624a16d3596f4b695a9c7b908d64ab
Zeno Rocha <zeno.rocha@liferay.com>
2014-04-19 19:09:25 -0300

    Move authors to a separated file

---
+++ authors.js
@@ -0,0 +1,95 @@
+module.exports = {
+
+    addy_osmani: {
+        name: "Addy Osmani",
+        bio: "Addy works on the Chrome Developer Relations team at Google, building and advocating for tools to help improve developer productivity and satisfaction. His personal projects include TodoMVC, which helps developers compare JavaScript MVC frameworks and AuraJS. He've also written 'Developing Backbone.js Applications' and 'Learning JavaScript Design Patterns'.",
+        gravatar: "96270e4c3e5e9806cf7245475c00b275",
+        gplus: "+AddyOsmani",
+        twitter: "addyosmani"
+    },
+    alex_komoroske: {
+        name: "Alex Komoroske",
+        bio: "Alex is a Product Manager on Chrome's Open Web Platform team. Before he was a product manager he was a web developer and even today loves building web apps in his spare time. Now he's excited to be helping other web developers make awesome apps that harness the open web platform's full potential.",
+        gravatar: "e6d71413617dfba7a5ff5b9a7180c007",
+        gplus: "+AlexKomoroske",
+        twitter: "jkomoros"
+    },
+    angelina_fabbro: {
+        name: "Angelina Fabbro",
+        bio: "Angelina is a developer from Vancouver, BC Canada working at Mozilla as a Technical Evangelist and developer advocate for Firefox OS. She loves JavaScript, web components, Node.js, mobile app development, and this cool place she hang out a lot called the world wide web. Oh, and let's not forget Firefox OS. In her spare time she takes singing lessons, play Magic: The Gathering, teach people to program, and collaborate with scientists for better programmer-scientist engagement.",
+        gravatar: "25bb0913435212f30f2fec0eb6e60dde",
+        gplus: "117339576748577555094",
+        twitter: "angelinamagnum"
+    },
+    community: {
+        name: "Community",
+        bio: "Posts marked as authored by the community represent best practices which have reached a level of concensus. They may evolve over time but their core fundamentals are considered generally good ideas.",
+        gravatar: "cedf2a3fe1ccf53aa00f50dda79cedf3",
+        gplus: "+WebComponents",
+        twitter: "webcomponents"
+    },
+    dominic_cooney: {
+        name: "Dominic Cooney",
+        bio: "Dominic is Staff Software Engineer at Google on web frontends and server and client web platforms.",
+        gravatar: "73b7633609b8f2bda1e73d35dabacb08",
+        gplus: "+DominicCooney",
+        twitter: "coonsta"
+    },
+    eric_bidelman: {
+        name: "Eric Bidelman",
+        bio: "Eric is a Staff Developer Programs Engineer on the Google Chrome team. His current passion is web components and Polymer. Eric is the author of 'Using the HTML5 Filesystem API' and regularly contributes to html5rocks.com and other open source projects. Prior to Google, Eric worked as a software engineer at the University of Michigan where he designed rich web applications and APIs for the university’s 19 libraries.",
+        gravatar: "e7948aac7c52b26470be80311873a398",
+        gplus: "+EricBidelman",
+        twitter: "ebidel"
+    },
+    mark_dalgleish: {
+        name: "Mark Dalgleish",
+        bio: "Mark is a senior UI developer at SEEK and the lead organiser of MelbJS. He’s obsessed with everything web and loves using JavaScript, CSS and HTML to create rich experiences that resonate with end users. In his spare time, he loves experimenting with the latest web technologies, sharing projects online and helping others learn progressive web development techniques.",
+        gravatar: "eec502b7f6f17f63f1e36d1cd566a254",
+        gplus: "104420396861285844168",
+        twitter: "markdalgleish"
+    },
+    peter_gasston: {
+        name: "Peter Gasston",
+        bio: "Peter Gasston has been a web developer for over 10 years in both agency and corporate settings. He was one of the original contributors to CSS3.info, the leading online destination for CSS3. Peter has been published in the UK's .net magazine, gives talks about CSS and web technologies at developer conferences, and runs the web development blog Broken Links. He lives in London, England.",
+        gravatar: "9d9f17e37b751b9f83081ba3dd44bab4",
+        gplus: "101356325339382110529",
+        twitter: "stopsatgreen"
+    },
+    rey_bango: {
+        name: "Rey Bango",
+        bio: "Rey is the Editor of Nettuts+ and works hard to serve up excellent technical articles and resources to help make web developers successful. He's also a developer evangelist at Telerik focused on helping developers building awesome experiences, an advocate for cross-browser, standards-based development and a former member of the jQuery Project Team.",
+        gravatar: "cf09578bc47ef46a1989b160374bc11d",
+        gplus: "+ReyBango",
+        twitter: "reybango"
+    },
+    rob_dodson: {
+        name: "Rob Dodson",
+        bio: "Rob is a Developer Advocate on the Google Chrome team. He is currently helping to educate developers about web components and Polymer. Though he's originally from the South, these days he divides his time between Mountain View and San Francisco.",
+        gravatar: "95c3a3b33ea51545229c625bef42e343",
+        gplus: "+RobDodson",
+        twitter: "rob_dodson"
+    },
+    marcy_sutton: {
+        name: "Marcy Sutton",
+        bio: "Developer @Substantial. @GDISeattle co-leader. Bike racer, music junkie, #a11y advocate, dog lover, big ole nerd. Also twittering @marcybikes.",
+        gravatar: "1cd529dea0aa4dda3b1242a350163a6e",
+        gplus: "118012571057666061540",
+        twitter: "marcysutton"
+    },
+    varunkumar_nagarajan: {
+        name: "Varunkumar Nagarajan",
+        bio: "A software engineer by profession and a blogger by passion. A web developer. Technology enthusiast. Have four years of industry experience.",
+        gravatar: "a7c860a23fc707909d7b78fcbb374367",
+        gplus: "+varunkumarnagarajan",
+        twitter: "varunkumar"
+    },
+    zeno_rocha: {
+        name: "Zeno Rocha",
+        bio: "Zeno is front-end engineer at Liferay, Inc. Besides that, he's a writer at Smashing Magazine, member at the Google Developer Experts program, host at Zone Of Front-Enders podcast and co-founder of BrazilJS Foundation.",
+        gravatar: "e190023b66e2b8aa73a842b106920c93",
+        gplus: "+ZenoRochaBN",
+        twitter: "zenorocha"
+    }
+
+}
--- contributing.md
+++ contributing.md
@@ -20,7 +20,7 @@ There are three main ways to contribute to this project.
 
 If you would like to submit an article or link to an article to the site, please follow these steps:
 
-* Create a new markdown (.html.md) file in `src/documents/articles` with the name of your article. Your filename should be lowercase and use dashes (-) to separate words. 
+* Create a new markdown (.html.md) file in `src/documents/articles` with the name of your article. Your filename should be lowercase and use dashes (-) to separate words.
 
 * At the top of the file, add a template block containing information about the article or post:
 
@@ -36,12 +36,12 @@ tags: ['Web Components']
 ---
 ```
 
-Please make sure to fill in the block with the `title`, `author`, `date` and so on. If you are only submitting a link to the article, please fill in `link` with the URL to your post. 
+Please make sure to fill in the block with the `title`, `author`, `date` and so on. If you are only submitting a link to the article, please fill in `link` with the URL to your post.
 
 If you are submitting a complete article for consideration, you don't need to include `link`. Instead, fill out the rest of the metadata and then below the closing `---` you can add in the content for your article.
 
-* Add your details (name, avatar, twitter handle etc) to docpad.js in the section labelled `authors`. This will be used by our templates to show your name and links to your profiles.
+* Add your details (name, avatar, twitter handle etc) to `authors.js`. This will be used by our templates to show your name and links to your profiles.
 
-* Use `npm run-script watch` and `npm run-script generate` to preview the output of your article. Please double-check that all formatting is as expected. 
+* Use `npm run-script watch` and `npm run-script generate` to preview the output of your article. Please double-check that all formatting is as expected.
 
 * You can now follow the development steps outlined earlier to submit a pull request for review :)
