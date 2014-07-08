20b900c848ffdb4d90614218155b46189ed5b770
Filipe Linhares <lip.linhares@gmail.com>
2014-07-08 07:23:06 -0300

    Add responsive menu - Fixes #131

---
+++ src/files/js/main.js
@@ -0,0 +1,14 @@
+(function (window, document, undefined) {
+
+    'use strict';
+
+    var button = document.querySelector('.menu-button'),
+        nav = document.querySelector('.nav'),
+        searchBox = document.querySelector('.search-box');
+
+    button.addEventListener('click', function(e) {
+        e.preventDefault();
+        nav.classList.toggle('desktop-only');
+        searchBox.classList.toggle('desktop-only');
+    });
+})(window, document);
