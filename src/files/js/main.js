c6fe86b142f7b9c8f76c59880617a907cb8f421e
Filipe Linhares <lip.linhares@gmail.com>
2014-07-08 10:48:00 -0300

    Fix code style

--- src/files/js/main.js
+++ src/files/js/main.js
@@ -2,10 +2,11 @@
 
     'use strict';
 
-    var button = document.querySelector('.menubutton'),
+    var button = document.querySelector('.menu-button'),
         nav = document.querySelector('.nav');
 
     button.addEventListener('click', function(e) {
+        e.preventDefault();
         nav.classList.toggle('desktop-only');
     });
 })(window, document);
