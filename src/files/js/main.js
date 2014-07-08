fe0b4b3fe6190b4f60de4b5be88a62af5fdbcf38
Filipe Linhares <lip.linhares@gmail.com>
2014-07-08 14:14:20 -0300

    Set visual changes

--- src/files/js/main.js
+++ src/files/js/main.js
@@ -3,10 +3,12 @@
     'use strict';
 
     var button = document.querySelector('.menu-button'),
-        nav = document.querySelector('.nav');
+        nav = document.querySelector('.nav'),
+        searchBox = document.querySelector('.search-box');
 
     button.addEventListener('click', function(e) {
         e.preventDefault();
         nav.classList.toggle('desktop-only');
+        searchBox.classList.toggle('desktop-only');
     });
 })(window, document);
