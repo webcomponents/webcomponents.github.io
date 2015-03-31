76972cc33a6985c11e56d4b8df189a7428c3e296
Beto Muniz <contato@betomuniz.com>
2015-03-31 00:44:43 -0300

    Applying observations of @zenorocha

--- src/files/js/events.js
+++ src/files/js/events.js
@@ -3,7 +3,8 @@
  Get Web Components Events from Meetup.com
 */
 
-(function () { 'use strict';
+(function() {
+    'use strict';
 
     function Events() {
         this.init();
@@ -13,16 +14,16 @@
 
         groups_source: "/data/groups.json",
 
-        init: function () {
+        init: function() {
             this.loadEvents();
         },
 
         loadEvents: function(events) {
             var that = this;
-            that._getData(that.groups_source, function(){
+            that._getData(that.groups_source, function() {
                 var data = JSON.parse(this.responseText),
                     groups = that.prepareGroups(data.groups, "id");
-                that._getData("https://api.meetup.com/2/events?&sign=true&photo-host=public&group_id="+groups+"&page=6&key=46455a723321385a1a58f4d77314c16", function() {
+                that._getData("https://api.meetup.com/2/events?&sign=true&photo-host=public&group_id=" + groups + "&page=6&key=46455a723321385a1a58f4d77314c16", function() {
                     var events = JSON.parse(this.responseText).results;
                     that.setEvents(events, data);
                 });
@@ -34,80 +35,116 @@
             var that = this,
                 eventsSection = document.querySelector("#events");
 
-                if(events.length > 0) {
-                    var eventCardTpl = document.querySelector("#card-event-template").innerHTML, virtualCardTpl = eventCardTpl,
-                        eventsOrdered = that.orderEvents(events),
-                        name,
-                        description,
-                        yes_rsvp_count,
-                        event_url;
+            if (events.length > 0) {
+                var eventCardTpl = document.querySelector("#card-event-template").innerHTML,
+                    virtualCardTpl = eventCardTpl,
+                    eventsOrdered = that.orderEvents(events),
+                    name,
+                    description,
+                    yes_rsvp_count,
+                    event_url;
 
-                    eventsSection.innerHTML = '';
-                    
-                    for (var i = 0; i < eventsOrdered.length; i++){
-                        virtualCardTpl = eventCardTpl;
+                eventsSection.innerHTML = '';
 
-                        // Add image to template
-                        virtualCardTpl = virtualCardTpl.replace(/{{image}}/g, that.getGroup(eventsOrdered[i].group.id, groups).thumbnail);
+                for (var i = 0; i < eventsOrdered.length; i++) {
+                    virtualCardTpl = eventCardTpl;
 
-                        // Add name to template
-                        name = (eventsOrdered[i].name) ? that._textStrip(eventsOrdered[i].name) : "";
-                        virtualCardTpl = virtualCardTpl.replace(/{{name}}/g, that._textTruncate(name));
+                    // Add image to template
+                    virtualCardTpl = that.setCardImage(virtualCardTpl, eventsOrdered[i], groups);
 
-                        // Add description to template
-                        description = (eventsOrdered[i].description) ? that._textStrip(eventsOrdered[i].description) : "This meetup has no description.";
-                        virtualCardTpl = virtualCardTpl.replace(/{{description}}/g, that._textTruncate(description, 170));
+                    // Add name to template
+                    virtualCardTpl = that.setCardName(virtualCardTpl, eventsOrdered[i]);
 
-                        // Convert timestamp to date and add to template
-                        virtualCardTpl = virtualCardTpl.replace(/{{date}}/g, that._timestampToDate(eventsOrdered[i].time));
+                    // Add description to template
+                    virtualCardTpl = that.setCardDescription(virtualCardTpl, eventsOrdered[i], "This meetup has no description.");
 
-                        if(eventsOrdered[i].venue){
-                            // Add city to template
-                            var city = (eventsOrdered[i].venue.city) ? eventsOrdered[i].venue.city+", " : "";
-                            virtualCardTpl = virtualCardTpl.replace(/{{city}}/g, city);
+                    // Add date to template
+                    virtualCardTpl = that.setCardDate(virtualCardTpl, eventsOrdered[i]);
 
-                            // Add country to template
-                            var country = (eventsOrdered[i].venue.country) ? eventsOrdered[i].venue.country : "";
-                            virtualCardTpl = virtualCardTpl.replace(/{{country}}/g, country);
-                        } else {
-                            virtualCardTpl = virtualCardTpl.replace(/{{city}}|{{country}}/g, "");
-                        }
+                    // Add venue to template
+                    virtualCardTpl = that.setCardVenue(virtualCardTpl, eventsOrdered[i]);
 
-                        // Add RVSPs to template
-                        yes_rsvp_count = (eventsOrdered[i].yes_rsvp_count && eventsOrdered[i].yes_rsvp_count > 0) ? eventsOrdered[i].yes_rsvp_count : "No RVSPs";
-                        virtualCardTpl = virtualCardTpl.replace(/{{yes_rsvp_count}}/g, yes_rsvp_count);
+                    // Add RVSPs to template
+                    virtualCardTpl = that.setCardRVSP(virtualCardTpl, eventsOrdered[i], "No RVSPs");
 
-                        // Add url to template
-                        event_url = (eventsOrdered[i].event_url) ? eventsOrdered[i].event_url : "";
-                        virtualCardTpl = virtualCardTpl.replace(/{{url}}/g, event_url);
-                        
-                        eventsSection.innerHTML += virtualCardTpl;
-                    }
-                } else {
-                    eventsSection.innerHTML = '<p class="no-events">No events found.</p>';
+                    // Add url to template and bind template
+                    eventsSection.innerHTML += that.setCardUrl(virtualCardTpl, eventsOrdered[i]);
                 }
+            } else {
+                eventsSection.innerHTML = '<p class="no-events">No events found.</p>';
+            }
 
         },
 
-        getGroup: function (group_id, data) {
+        setCardImage: function(virtualTpl, eventsOrdered, groups) {
+            return this.prepareTpl(virtualTpl, 'image', this.getGroup(eventsOrdered.group.id, groups).thumbnail);
+        },
+
+        setCardName: function(virtualTpl, eventsOrdered) {
+            var name = (eventsOrdered.name) ? this._textStrip(eventsOrdered.name) : "";
+            return this.prepareTpl(virtualTpl, 'name', this._textTruncate(name));
+        },
+
+        setCardDescription: function(virtualTpl, eventsOrdered, defaultMsg) {
+            var description = (eventsOrdered.description) ? this._textStrip(eventsOrdered.description) : defaultMsg;
+            return this.prepareTpl(virtualTpl, 'description', this._textTruncate(description, 170));
+        },
+
+        setCardDate: function(virtualTpl, eventsOrdered) {
+            return this.prepareTpl(virtualTpl, 'date', this._timestampToDate(eventsOrdered.time));
+        },
+
+        setCardVenue: function(virtualTpl, eventsOrdered) {
+            if (eventsOrdered.venue) {
+                // Add city to template
+                var city = (eventsOrdered.venue.city) ? eventsOrdered.venue.city + ", " : "";
+                virtualTpl = this.prepareTpl(virtualTpl, 'city', city);
+
+                // Add country to template
+                var country = (eventsOrdered.venue.country) ? eventsOrdered.venue.country : "";
+                virtualTpl = this.prepareTpl(virtualTpl, 'country', country);
+            } else {
+                virtualTpl = this.prepareTpl(virtualTpl, 'country', "");
+            }
+            return virtualTpl;
+        },
+
+        setCardRVSP: function(virtualTpl, eventsOrdered, defaultMsg) {
+            var yes_rsvp_count = (eventsOrdered.yes_rsvp_count && eventsOrdered.yes_rsvp_count > 0) ? eventsOrdered.yes_rsvp_count : defaultMsg;
+            return this.prepareTpl(virtualTpl, 'yes_rsvp_count', yes_rsvp_count);
+        },
+
+        setCardUrl: function(virtualTpl, eventsOrdered) {
+            var event_url = (eventsOrdered.event_url) ? eventsOrdered.event_url : "";
+            return this.prepareTpl(virtualTpl, 'url', event_url);
+        },
+
+        getGroup: function(group_id, data) {
             var group;
             for (var i = 0; i < data.groups.length; i++) {
-                if(data.groups[i].id == group_id) group = data.groups[i];
+                if (data.groups[i].id == group_id) group = data.groups[i];
             };
             return group;
         },
 
-        prepareGroups: function (obj, key) {
-            return obj.map(function(i){ return i[key] }).toString();
+        prepareTpl: function(virtualTpl, key, value) {
+            var re = new RegExp("{{" + key + "}}", "g");
+            return virtualTpl.replace(re, value);
+        },
+
+        prepareGroups: function(obj, key) {
+            return obj.map(function(i) {
+                return i[key]
+            }).toString();
         },
 
         orderEvents: function(events) {
-            return events.sort(function(x, y){
+            return events.sort(function(x, y) {
                 return x.time - y.time;
             });
         },
 
-        _getData: function (url, callback) {
+        _getData: function(url, callback) {
             var req = new XMLHttpRequest();
             req.onload = callback;
             req.open("GET", url, true);
@@ -115,20 +152,21 @@
         },
 
         _textStrip: function(html) {
-            html = html.replace(/\n/gm,'');
-            return html.replace(/<(?:.|\n)*?>/gm,'');
+            html = html.replace(/\n/gm, '');
+            return html.replace(/<(?:.|\n)*?>/gm, '');
         },
 
         _timestampToDate: function(ts) {
-            var t = new Date(ts), day, month, date;
-            day = (t.getDate() <= 9) ? "0"+t.getDate() : t.getDate();
-            month = ((t.getMonth() + 1) <= 9) ? "0"+(t.getMonth() + 1) : (t.getMonth() + 1);
-            date = month+"/"+day;
+            var t = new Date(ts),
+                day, month, date;
+            day = (t.getDate() <= 9) ? "0" + t.getDate() : t.getDate();
+            month = ((t.getMonth() + 1) <= 9) ? "0" + (t.getMonth() + 1) : (t.getMonth() + 1);
+            date = month + "/" + day;
             return date;
         },
 
         _textTruncate: function(text, size) {
-            return (text.length > size) ? text.substring(0, size)+"..." : text;
+            return (text.length > size) ? text.substring(0, size) + "..." : text;
         }
     };
 
