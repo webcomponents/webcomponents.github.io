98212d47170cc425b587e09455f23dcca0dd01f0
Nevraeka <nevraeka@gmail.com>
2015-07-27 09:18:53 -0400

    [#230] Updated community card

---
+++ contributors.js
@@ -0,0 +1,208 @@
+module.exports = {
+    "contributors" : [
+        {
+            name : "Zeno Rocha",
+            twitter : "zenorocha",
+            gravatar : "e190023b66e2b8aa73a842b106920c93"
+
+        },
+
+        {
+            name : "Addy Osmani",
+            twitter : "addyosmani",
+            gravatar : "96270e4c3e5e9806cf7245475c00b275"
+        },
+
+        {
+            name : "Erik Isaksen",
+            twitter : "eisaksen",
+            image : "https://avatars0.githubusercontent.com/u/647043?v=3&amp;s=80"
+        },
+        {
+            name: "Mateus Ortiz",
+            twitter: "mateusortiz",
+            image: "https://avatars1.githubusercontent.com/u/3077085?v=3&s=80"
+        },
+
+        {
+            name : "Luan Muniz",
+            twitter: "lluanmuniz",
+            image: "https://avatars0.githubusercontent.com/u/3428149?v=3&s=80"
+        },
+
+        {
+            name: "Pascal Precht",
+            twitter: "PascalPrecht",
+            gravatar: "b32bdb1fc9fdadeb45d7a1267fdd2fc4"
+        },
+
+        {
+            name : "Eduardo Lundgren",
+            twitter: "eduardolungren",
+            gravatar: "42327de520e674a6d1686845b30778d0"
+        },
+
+        {
+            name : "Beto Muniz",
+            twitter: "obetomuniz",
+            image: "https://avatars1.githubusercontent.com/u/1680157?v=3&s=80"
+        },
+
+        {
+            name: "Ciro Nunes",
+            twitter: "cironunesdev",
+            image: "https://avatars2.githubusercontent.com/u/469908?v=3&s=80"
+        },
+
+        {
+            name: "Sindre Sorhus",
+            twitter: "sindresorhus",
+            image: "d36a92237c75c5337c17b60d90686bf9"
+        },
+
+        {
+            name: "Rob Dodson",
+            twitter: "rob_dodson",
+            gravatar: "95c3a3b33ea51545229c625bef42e343"
+        },
+
+
+        {
+            name: "Bernard De Luna",
+            twitter: "bernarddeluna",
+            gravatar: "bc16c9be1e05e65395487b78b1cc72c0"
+        },
+
+        {
+            name: "Mathias Bynens",
+            twitter: "mathias",
+            image: "https://avatars2.githubusercontent.com/u/81942?v=3&s=80"
+        },
+
+        {
+            name: "Bruno Passos",
+            twitter: "brunopassos",
+            image: "https://avatars3.githubusercontent.com/u/1923332?v=3&s=80"
+        },
+
+        {
+            name: "Randson Oliveira",
+            twitter: "randsonjs",
+            image: "https://avatars2.githubusercontent.com/u/4191734?v=3&s=80"
+        },
+
+        {
+            name: "Jan Miksovsky",
+            twitter: "JanMiksovsky",
+            image: "https://avatars0.githubusercontent.com/u/86139?v=3&s=80"
+        },
+
+        {
+            name: "Taylor Savage",
+            twitter: "TaylorTheSavage",
+            image: "https://avatars2.githubusercontent.com/u/348551?v=3&s=80"
+        },
+
+        {
+            name: "kiko01b",
+            twitter: "kiko01b",
+            image: "https://avatars0.githubusercontent.com/u/873000?v=3&s=80"
+        },
+
+        {
+            name: "Filipe Linhares",
+            twitter: "ofilipelinhares",
+            image: "https://avatars3.githubusercontent.com/u/2338778?v=3&s=80"
+        },
+
+        {
+            name: "Pascal Borreli",
+            twitter: "pborreli",
+            image: "https://avatars2.githubusercontent.com/u/77759?v=3&s=80"
+        },
+
+        {
+            name: "Ryuichi Okumura",
+            twitter: "okuryu",
+            image: "https://avatars3.githubusercontent.com/u/34588?v=3&s=80"
+        },
+
+        {
+            name: "Cătălin Mariș",
+            twitter: "alrra",
+            image: "https://avatars0.githubusercontent.com/u/1223565?v=3&s=80"
+        },
+
+        {
+            name: "Sébastien Cevey",
+            twitter: "theefer",
+            image: "https://avatars0.githubusercontent.com/u/36964?v=3&s=80"
+        },
+
+        {
+            name: "Christian Oliff",
+            twitter: "christianoliff",
+            image: "https://avatars3.githubusercontent.com/u/1212885?v=3&s=80"
+        },
+
+        {
+            name: "Philip Walton",
+            twitter: "philwalton",
+            image: "https://avatars1.githubusercontent.com/u/326742?v=3&s=80"
+        },
+
+        {
+            name: "Varunkumar Nagarajan",
+            twitter: "varunkumar",
+            image: "https://avatars3.githubusercontent.com/u/509433?v=3&s=80"
+        },
+
+        {
+            name: "Andrew Rota",
+            twitter: "AndrewRota",
+            image: "https://avatars1.githubusercontent.com/u/718017?v=3&s=80"
+        },
+
+        {
+            name: "Breno Polanski",
+            twitter: "brenopolanski",
+            image: "https://avatars3.githubusercontent.com/u/1894191?v=3&s=80"
+        },
+
+        {
+            name: "Seth Ladd",
+            twitter: "sethladd",
+            image: "https://avatars2.githubusercontent.com/u/5479?v=3&s=80"
+        },
+
+        {
+            name: "Raphaël Rougeron",
+            twitter: "goldoraf",
+            image: "https://avatars0.githubusercontent.com/u/5461?v=3&s=80"
+        },
+
+        {
+            name: "Rolandas Barysas",
+            twitter: "rbrsio",
+            image: "https://avatars3.githubusercontent.com/u/1409998?v=3&s=80"
+        },
+
+        {
+            name: "Eric Bidelman",
+            twitter: "ebidel",
+            gravatar: "e7948aac7c52b26470be80311873a398"
+        },
+
+        {
+            name: "Briza Bueno",
+            twitter: "brizabueno",
+            gravatar: "c272a0a8972473fdf231f2b2d897c242"
+        },
+
+        {
+            name: "Alex Komoroske",
+            twitter: "jkomoros",
+            image: "https://lh5.googleusercontent.com/-WlbCSDZ9t5Y/AAAAAAAAAAI/AAAAAAAAC2M/A_HM2ovbT2o/s120-c/photo.jpg"
+        }
+    ]
+};
