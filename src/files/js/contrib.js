b00351cf310b809bb55851032444bc0d68584f43
Zeno Rocha <hi@zenorocha.com>
2015-08-24 10:25:50 -0700

    Removes unnecessary variables and optimize image loading for About page #290

--- src/files/js/contrib.js
+++ src/files/js/contrib.js
@@ -2,34 +2,26 @@
 
     'use strict';
 
-    var baseUrl = 'https://api.github.com/';
-
     // Callback function
     function appendContributors() {
         /*jshint validthis: true*/
         var result = JSON.parse(this.responseText),
-            i,
-            length = result.length,
             tpl = '<li class="contributor" itemprop="itemListElement" itemscope itemtype="http://schema.org/Person"><a href="#"><img class="picture" width="90" height="90"></a></li>',
             str = '',
-            obj,
             div;
 
-        for (i = 0; i < length; i += 1) {
-            obj = result[i];
-
+        for (var i = 0; i < result.length; i++) {
             div = document.createElement('div');
             div.innerHTML = tpl;
-            div.querySelector('a').href = obj.html_url;
-            div.querySelector('img').src = obj.avatar_url;
-            // document.body.querySelector('.users').appendChild(div);
+            div.querySelector('a').href = result[i].html_url;
+            div.querySelector('img').src = result[i].avatar_url + '&size=180';
             str += div.innerHTML;
         }
 
-        document.body.querySelector('.profile-list').innerHTML = str;
+        document.querySelector('.profile-list').innerHTML = str;
     }
 
-    var url = baseUrl + "repos/webcomponents/webcomponents.github.io/contributors";
+    var url = "https://api.github.com/repos/webcomponents/webcomponents.github.io/contributors";
 
     var oReq = new XMLHttpRequest();
     oReq.addEventListener('load', appendContributors);
